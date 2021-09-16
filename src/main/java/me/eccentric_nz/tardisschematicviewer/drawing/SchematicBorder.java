575ef479552b7661057e3cdd4d91361dd07535ad
DalekCraft <ericdalekcraft@gmail.com>
2021-09-16 12:33:27 -0400

    Todo thing

--- src/main/java/me/eccentric_nz/tardisschematicviewer/drawing/SchematicBorder.java
+++ src/main/java/me/eccentric_nz/tardisschematicviewer/drawing/SchematicBorder.java
@@ -10,9 +10,7 @@ public class SchematicBorder {
     public static void draw(GL4bc gl, float sizeX, float sizeY, float sizeZ) {
         gl.glBegin(GL.GL_LINES);
 
-        // float[] fullBright = {1.0f, 1.0f, 1.0f, 1.0f};
-
-        // gl.glLightModelfv(GL_LIGHT_MODEL_AMBIENT, fullBright, 0);
+        // TODO Make this fullbright, regardless of camera angle.
 
         gl.glColor3f(1.0f, 0.0f, 0.0f);
         gl.glVertex3f(-sizeX, -sizeY, -sizeZ);
