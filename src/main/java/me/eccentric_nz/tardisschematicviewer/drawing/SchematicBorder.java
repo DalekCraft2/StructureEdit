314c34a0596fd8c07d6bc9166e31f64f66def595
DalekCraft <ericdalekcraft@gmail.com>
2021-09-19 22:34:46 -0400

    Clean schematic border code

--- src/main/java/me/eccentric_nz/tardisschematicviewer/drawing/SchematicBorder.java
+++ src/main/java/me/eccentric_nz/tardisschematicviewer/drawing/SchematicBorder.java
@@ -7,21 +7,25 @@ import static com.jogamp.opengl.GL4bc.GL_LINES;
 public class SchematicBorder {
 
     public static void draw(GL4bc gl, float sizeX, float sizeY, float sizeZ) {
+        gl.glTranslatef(-sizeX / 2.0f, -sizeY / 2.0f, -sizeZ / 2.0f);
+
         gl.glLineWidth(2.0f);
         gl.glBegin(GL_LINES);
 
         gl.glColor3f(1.0f, 0.0f, 0.0f);
-        gl.glVertex3f(-sizeX / 2.0f, -sizeY / 2.0f, -sizeZ / 2.0f);
-        gl.glVertex3f(sizeX / 2.0f, -sizeY / 2.0f, -sizeZ / 2.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(sizeX, 0.0f, 0.0f);
 
         gl.glColor3f(0.0f, 1.0f, 0.0f);
-        gl.glVertex3f(-sizeX / 2.0f, -sizeY / 2.0f, -sizeZ / 2.0f);
-        gl.glVertex3f(-sizeX / 2.0f, sizeY / 2.0f, -sizeZ / 2.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, sizeY, 0.0f);
 
         gl.glColor3f(0.0f, 0.0f, 1.0f);
-        gl.glVertex3f(-sizeX / 2.0f, -sizeY / 2.0f, -sizeZ / 2.0f);
-        gl.glVertex3f(-sizeX / 2.0f, -sizeY / 2.0f, sizeZ / 2.0f);
+        gl.glVertex3f(0.0f, 0.0f, 0.0f);
+        gl.glVertex3f(0.0f, 0.0f, sizeZ);
 
         gl.glEnd();
+
+        gl.glTranslatef(sizeX / 2.0f, sizeY / 2.0f, sizeZ / 2.0f);
     }
 }
